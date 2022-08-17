aacceae699097b972b82fa86913d919ec2838da8
Andras Kosik <proand100@pm.me>
2022-08-17 02:51:24 +0200

    After the imgGifPlan.php get and set functions.

---
+++ gifProject.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'imgGifPlan.php';
+class gifProj{
+
+$mainImg = new gifPlan();
+
+
+
+
+
+}
+
+?>
+
+
---
+++ imgGifPlan.php
@@ -0,0 +1,86 @@
+<?php
+class gifPlan{
+    public $szakaszokSzama = 0;
+    public $imgSRC;
+    public $tartam; // array is lehet
+public $delay;  // array is lehet
+    public $startX; // array is lehet
+    public $startY; // array is lehet
+    public $endX; // array is lehet
+    public $endY; // array is lehet
+    public $startFrame; // array is lehet
+    public $endFrame;  // array is lehet
+    public $alakitasMod;  // array is lehet, 1=mozgat, 2=light, 3=opacity
+    public $startOPacity;  // array is lehet
+    public $endOpacity;  // array is lehet
+    public $startLight;  // array is lehet
+    public $endLight;  // array is lehet
+    
+    function setszakaszokSzama(){
+       $this->szakaszokSzama++;
+     }
+    
+     function getSzakaszokSzama(){
+       return $this->szakaszokSzama;
+     }
+
+ function setIdoTartam($Be){
+      /// $GLOBALS['tartam'][$GLOBALS['szakaszokSzama']] = $Be;
+ $this->tartam[$this->szakaszokSzama] = $Be;
+
+ }
+ function setimgSRC($Be){
+    $this->imgSRC = $Be; // ?
+ }
+ 
+ function setdelay($Be){
+    $this->delay[$this->szakaszokSzama] = $Be;
+ }
+ function setstartX($Be){
+    $this->startX[$this->szakaszokSzama] = $Be;
+ }
+
+ function setstartY($Be){
+    $this->startY[$this->szakaszokSzama] = $Be;
+ }
+ function setendX($Be){
+    $this->endX[$this->szakaszokSzama] = $Be;
+ }
+ function setendY($Be){
+    $this->endY[$this->szakaszokSzama] = $Be;
+ }
+ function setstartFrame($Be){
+    $this->startFrame[$this->szakaszokSzama] = $Be;
+ }
+ function setendFrame($Be){
+    $this->endFrame[$this->szakaszokSzama] = $Be;
+ }
+
+
+ function setalakitasMod($Be){
+    $this->alakitasMod[$this->szakaszokSzama] = $Be;
+ }
+ function setstartOPacity($Be){
+    $this->startOPacity[$this->szakaszokSzama] = $Be;
+ }
+ 
+ function setendOpacity($Be){
+    $this->endOpacity[$this->szakaszokSzama] = $Be;
+ }
+ function setstartLight($Be){
+    $this->startLight[$this->szakaszokSzama] = $Be;
+ }
+
+ function setendLight($Be){
+    $this->endLight[$this->szakaszokSzama]  = $Be;
+ }
+
+
+
+
+}
+
+?>
+
+
+
